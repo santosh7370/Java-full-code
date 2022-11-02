01c10da43e7f79130ccd79ec17af43ef8d6742db
Santosh Singhaniya <116973860+santosh7370@users.noreply.github.com>
2022-11-02 22:30:08 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,15 @@
+
+class A
+{
+    public static void main(String[] args)
+    {
+        int a,b,c;
+        System.out.println("Enter the two number");
+        Scanner s=new Scanner();
+        a=s.nextInt();
+        
+        b=s.nextInt();
+        c=a+b;
+        System.out.println("Sum of two number");
+    }
+}
